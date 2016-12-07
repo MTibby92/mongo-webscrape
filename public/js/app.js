c73dd3ff1756ccc864660cd8be41978faace26df
MTibby92 <MTibby92@gmail.com>
2016-12-07 17:05:48 -0600

    ajax calls working, and previous and next article buttons working along with counter

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,20 @@ $( document ).ready(function() {
             $('#data-title').text('Article ' + counter)
             $('#data-header').text(data.title)
             $('#data-link').text(data.link)
-            counter++  
+            // counter++  
+        }).done(function(response) {
+            getNext(response)
+            getPrevious(response)
+        })
+    }
+
+    function displayUpdate(id) {
+        $.getJSON('/latest/' + id, function(data) {
+            console.log(data)
+            $('#data-title').text('Article ' + counter)
+            $('#data-header').text(data.title)
+            $('#data-link').text(data.link)
+            // counter++  
         }).done(function(response) {
             getNext(response)
             getPrevious(response)
@@ -34,6 +47,8 @@ $( document ).ready(function() {
             if (data2 !== null) {
                 $('#previous-article').attr('data-db-id', data2._id)
                 $('#previous-article').removeClass('disabled')
+            } else {
+                $('#previous-article').addClass('disabled')
             }
         })
     }
@@ -50,10 +65,18 @@ $( document ).ready(function() {
 
     $('#next-article').on('click', function(event) {
     	console.log('next article please')
+        counter++
+        dbID = $('#next-article').attr('data-db-id')
+        console.log('data-db-id:', dbID)
+        displayUpdate(dbID)
     })
 
     $('#previous-article').on('click', function(event) {
     	console.log('previous article please')
+        counter--
+        dbID = $('#previous-article').attr('data-db-id')
+        console.log('data-db-id:', dbID)
+        displayUpdate(dbID)
     })
 
     $('a[href^="#"]:not([href="#/"])').on('click',function (e) {
