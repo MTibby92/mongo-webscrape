dd2d54c0e2588e03dde8c26b1654192afc3e3124
MTibby92 <MTibby92@gmail.com>
2016-12-08 10:44:45 -0600

    prevented upload and delete buttons from doing anything if no input or note to delete

--- public/js/app.js
+++ public/js/app.js
@@ -72,29 +72,34 @@ $( document ).ready(function() {
 
     $('#upload-icon').on('click', function(event) {
     	console.log('upload clicked')
-        var dbID = $('#data-header').attr('data-db-id')
-        console.log(dbID)
-        console.log($('#note-area').val())
-        $.post('/addNote/' + dbID, {body: $('#note-area').val()})
-        .done(function(data) {
-            console.log(data)
-            console.log('post complete')
-            $('#note-area').val('')
-            var id = $('#data-header').attr('data-db-id')
-            console.log(id)
-            $.getJSON('/latest/' + id)
-            .done(function(data2) {
-                console.log(data2)
-                $('#display-saved-note').text(data2.note.body)
-                $('#display-saved-note').attr('data-note-id', data2.note._id)
+        // check if anything was entered into the text area; do nothing if empty
+        if (!$('#note-area').val().trim()) {
+            console.log('Nothing to upload')
+        } else {
+            var dbID = $('#data-header').attr('data-db-id')
+            console.log(dbID)
+            console.log($('#note-area').val())
+            $.post('/addNote/' + dbID, {body: $('#note-area').val()})
+            .done(function(data) {
+                console.log(data)
+                console.log('post complete')
+                $('#note-area').val('')
+                var id = $('#data-header').attr('data-db-id')
+                console.log(id)
+                $.getJSON('/latest/' + id)
+                .done(function(data2) {
+                    console.log(data2)
+                    $('#display-saved-note').text(data2.note.body)
+                    $('#display-saved-note').attr('data-note-id', data2.note._id)
+                })
+                .fail(function(err) {
+                    console.log(err)
+                })
             })
             .fail(function(err) {
                 console.log(err)
             })
-        })
-        .fail(function(err) {
-            console.log(err)
-        })
+        }    
     })
 
 // Experimental Code Block; error appears to be with the server side deletion script
@@ -128,14 +133,19 @@ $( document ).ready(function() {
 
     $('#remove-icon').on('click', function(event) {
     	console.log('remove clicked')
-        var noteID = $('#display-saved-note').attr('data-note-id') 
-        $.post('/deleteNote/' + noteID)
-        .done(function(data) {
-            $('#display-saved-note').text('This is where a note would be')
-            $('#display-saved-note').attr('data-note-id', '')
-            console.log(data)
-            console.log('Deletion Complete')
-        })       
+        var noteID = $('#display-saved-note').attr('data-note-id')
+        // check if there's a note displayed; do nothing if there's no note there
+        if(!noteID) {
+            console.log('No note to remove')
+        } else {
+            $.post('/deleteNote/' + noteID)
+            .done(function(data) {
+                $('#display-saved-note').text('This is where a note would be')
+                $('#display-saved-note').attr('data-note-id', '')
+                console.log(data)
+                console.log('Deletion Complete')
+            })
+        }
     })
 
     $('#next-article').on('click', function(event) {
